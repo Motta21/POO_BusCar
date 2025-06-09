a3a952ba438caab0c62f60a0289dbca328c3b507
Felipe-fMoura <fmoura.dev@gmail.com>
2025-06-09 15:54:59 -0300

    feat: implementação tela passageiros/alunos

---
+++ POO_Buscarr/view/TelaPassageiros.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace POO_Buscarr.view
+{
+    partial class TelaPassageiros
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.painelTopo = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.dgvPassageiros = new System.Windows.Forms.DataGridView();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.painelTopo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPassageiros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // painelTopo
+            // 
+            this.painelTopo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.painelTopo.Controls.Add(this.lblTitulo);
+            this.painelTopo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.painelTopo.Location = new System.Drawing.Point(0, 0);
+            this.painelTopo.Name = "painelTopo";
+            this.painelTopo.Size = new System.Drawing.Size(1920, 80);
+            this.painelTopo.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(30, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(486, 45);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "🚍 Gerenciador de Passageiros";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtBuscar.Location = new System.Drawing.Point(50, 100);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(400, 29);
+            this.txtBuscar.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            this.btnBuscar.BackColor = System.Drawing.Color.Gold;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnBuscar.Location = new System.Drawing.Point(470, 98);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(120, 35);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.BackColor = System.Drawing.Color.Gold;
+            this.btnAtualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAtualizar.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnAtualizar.Location = new System.Drawing.Point(600, 98);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(120, 35);
+            this.btnAtualizar.TabIndex = 3;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = false;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // dgvPassageiros
+            // 
+            this.dgvPassageiros.AllowUserToAddRows = false;
+            this.dgvPassageiros.AllowUserToDeleteRows = false;
+            this.dgvPassageiros.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPassageiros.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvPassageiros.EnableHeadersVisualStyles = false;
+            this.dgvPassageiros.Location = new System.Drawing.Point(50, 150);
+            this.dgvPassageiros.Name = "dgvPassageiros";
+            this.dgvPassageiros.ReadOnly = true;
+            this.dgvPassageiros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPassageiros.Size = new System.Drawing.Size(1820, 800);
+            this.dgvPassageiros.TabIndex = 4;
+            // 
+            // btnExportar
+            // 
+
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            this.btnExportar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.btnExportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExportar.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnExportar.ForeColor = System.Drawing.Color.White;
+            this.btnExportar.Location = new System.Drawing.Point(50, 970);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(160, 40);
+            this.btnExportar.TabIndex = 5;
+            this.btnExportar.Text = "Exportar Lista";
+            this.btnExportar.UseVisualStyleBackColor = false;
+            // 
+            // btnVoltar
+            // 
+
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            this.btnVoltar.BackColor = System.Drawing.Color.LightGray;
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.btnVoltar.Location = new System.Drawing.Point(1710, 970);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(160, 40);
+            this.btnVoltar.TabIndex = 6;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = false;
+            // 
+            // TelaPassageiros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1920, 1061);
+            this.Controls.Add(this.painelTopo);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.dgvPassageiros);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnVoltar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "TelaPassageiros";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Visualizar Passageiros";
+            this.painelTopo.ResumeLayout(false);
+            this.painelTopo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPassageiros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Panel painelTopo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.DataGridView dgvPassageiros;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.Button btnVoltar;
+    }
+}
---
+++ POO_Buscarr/view/TelaPassageiros.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using POO_Buscarr.model;
+
+namespace POO_Buscarr.view
+{
+    public partial class TelaPassageiros : Form
+    {
+        private readonly string connectionString = "Server=127.0.0.1;Database=buscard;Uid=root;Pwd=;";
+
+        public TelaPassageiros()
+        {
+            InitializeComponent();
+            ConfigurePlaceholder();
+            ConfigureDataGridView();
+            CarregarPassageiros();
+        }
+
+        private void ConfigurePlaceholder()
+        {
+            txtBuscar.Text = "Buscar por nome...";
+            txtBuscar.ForeColor = Color.Gray;
+            txtBuscar.Enter += (s, e) =>
+            {
+                if (txtBuscar.Text == "Buscar por nome...")
+                {
+                    txtBuscar.Text = "";
+                    txtBuscar.ForeColor = Color.Black;
+                }
+            };
+            txtBuscar.Leave += (s, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(txtBuscar.Text))
+                {
+                    txtBuscar.Text = "Buscar por nome...";
+                    txtBuscar.ForeColor = Color.Gray;
+                }
+            };
+        }
+
+        private void ConfigureDataGridView()
+        {
+            dgvPassageiros.AutoGenerateColumns = false;
+            dgvPassageiros.AllowUserToAddRows = false;
+            dgvPassageiros.AllowUserToDeleteRows = false;
+            dgvPassageiros.ReadOnly = true;
+            dgvPassageiros.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            // Configurar colunas
+            dgvPassageiros.Columns.Add("Nome", "Nome");
+            dgvPassageiros.Columns.Add("Idade", "Idade");
+            dgvPassageiros.Columns.Add("Endereco", "Endereço");
+            dgvPassageiros.Columns.Add("Escola", "Escola");
+            dgvPassageiros.Columns.Add("Responsavel", "Responsável");
+            dgvPassageiros.Columns.Add("Telefone", "Telefone");
+        }
+
+        private void CarregarPassageiros()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT nome_Aluno, idade, endereco, escola, nome_Responsavel, telefone_Responsavel FROM aluno";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        dgvPassageiros.Rows.Clear();
+
+                        while (reader.Read())
+                        {
+                            dgvPassageiros.Rows.Add(
+                                reader["nome_Aluno"]?.ToString() ?? "",
+                                int.TryParse(reader["idade"]?.ToString(), out int idade) ? idade.ToString() : "0",
+                                reader["endereco"]?.ToString() ?? "",
+                                reader["escola"]?.ToString() ?? "",
+                                reader["nome_Responsavel"]?.ToString() ?? "",
+                                reader["telefone_Responsavel"]?.ToString() ?? ""
+                            );
+                        }
+                    }
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show($"Erro de formato nos dados: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar passageiros: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string termoBusca = txtBuscar.Text.Trim();
+
+            if (string.IsNullOrEmpty(termoBusca) || termoBusca == "Buscar por nome...")
+            {
+                CarregarPassageiros();
+                return;
+            }
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT nome_Aluno, idade, endereco, escola, nome_Responsavel, telefone_Responsavel " +
+                                   "FROM aluno WHERE nome_Aluno LIKE @termo";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@termo", $"%{termoBusca}%");
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        dgvPassageiros.Rows.Clear();
+
+                        while (reader.Read())
+                        {
+                            dgvPassageiros.Rows.Add(
+                                reader["nome_Aluno"].ToString(),
+                                reader["idade"].ToString(),
+                                reader["endereco"].ToString(),
+                                reader["escola"].ToString(),
+                                reader["nome_Responsavel"].ToString(),
+                                reader["telefone_Responsavel"].ToString()
+                            );
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao buscar passageiros: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            CarregarPassageiros();
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Arquivo CSV (*.csv)|*.csv";
+                saveFileDialog.Title = "Exportar lista de passageiros";
+                saveFileDialog.FileName = $"passageiros_{DateTime.Now:yyyyMMdd}.csv";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(saveFileDialog.FileName))
+                    {
+                        // Cabeçalho
+                        file.WriteLine("Nome,Idade,Endereço,Escola,Responsável,Telefone");
+
+                        // Dados
+                        foreach (DataGridViewRow row in dgvPassageiros.Rows)
+                        {
+                            if (!row.IsNewRow)
+                            {
+                                file.WriteLine(
+                                    $"\"{row.Cells[0].Value}\"," +
+                                    $"\"{row.Cells[1].Value}\"," +
+                                    $"\"{row.Cells[2].Value}\"," +
+                                    $"\"{row.Cells[3].Value}\"," +
+                                    $"\"{row.Cells[4].Value}\"," +
+                                    $"\"{row.Cells[5].Value}\""
+                                );
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Lista exportada com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao exportar lista: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
